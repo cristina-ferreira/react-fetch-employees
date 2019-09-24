a019da9fe794a5ecb2f015a2063b6cc8660c42a9
cristinaferreira <cristina.d.ferreira@sapo.pt>
2019-09-24 15:34:54 +0100

    Updates

---
+++ precache-manifest.8f6bd3032610c90fac6048e814d2b9f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5383ae52e9fc29d406442aa87a89de",
+    "url": "/react-fetch-employees/index.html"
+  },
+  {
+    "revision": "22428e673b3ab07478b9",
+    "url": "/react-fetch-employees/static/css/main.940e678b.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react-fetch-employees/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "22428e673b3ab07478b9",
+    "url": "/react-fetch-employees/static/js/main.5092cdf5.chunk.js"
+  },
+  {
+    "revision": "6c3df9e79ce8fe2cf8bb",
+    "url": "/react-fetch-employees/static/js/runtime~main.8243bd93.js"
+  }
+]);
